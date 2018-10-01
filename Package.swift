07940b5e9c4281dc46c96d704bb4c657bfcccd43
Vladyslav Popovych <mailuatc@gmail.com>
2018-10-01 19:53:42 +0300

    ~ Fix

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:4.0
 /*
  * Package.swift
  * Copyright (c) 2015 Ben Gollmer.
@@ -19,5 +20,11 @@ import PackageDescription
 
 let package = Package(
   name: "CommandLine",
-  exclude: ["script"]
+
+  targets: [
+    .target(
+      name: "CommandLine",
+      path: "CommandLineKit"
+    ),
+  ]
 )
